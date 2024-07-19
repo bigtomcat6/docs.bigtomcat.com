36e6fd6156a0200635523aa71bb63621529d85ab
bigtomcat6 <bigtomcat@qq.com>
2024-07-19 14:59:18 +1000

    feat: Add Component - HSentence

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,7 +1,12 @@
 import { defineClientConfig } from "vuepress/client";
 import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
 
+import HSentence from "./components/HSentence.vue";
+
 export default defineClientConfig({
+  enhance({app}) {
+    app.component("HSentence", HSentence);
+  },
   setup: () => {
     setupTransparentNavbar({ 
       type: "homepage" || "blog-homepage",
