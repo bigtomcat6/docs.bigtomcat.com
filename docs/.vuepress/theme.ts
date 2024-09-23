478070b04f64859fe28c3cd00b2694d0c5a2829e
bigtomcat6 <bigtomcat@qq.com>
2024-09-24 02:51:53 +1000

    feat(theme): Update searchPro hotkeys in theme.ts

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -136,7 +136,12 @@ export default hopeTheme({
       components: ["Badge", "VPCard"],
     },
 
-    searchPro: true,
+    searchPro: {
+      hotKeys: [
+        { key: "k", ctrl: true },
+        { key: "k", meta: true },
+      ]
+    },
 
     // 此处开启了很多功能用于演示，你应仅保留用到的功能。
     mdEnhance: {
